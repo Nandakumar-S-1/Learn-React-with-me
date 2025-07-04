b16fff486e106124c2e101dc73a232346547917e
Nandu <nandakumars5352@gmail.com>
2025-07-04 19:06:18 +0530

    react lazy

--- react-Hooks/src/Lazy/LazyLoading.jsx
+++ react-Hooks/src/Lazy/LazyLoading.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{Suspense, useState} from 'react'
 
 const Contact =React.lazy(()=>import('./Contact'))
 const Login = React.lazy(()=>import('./login'))
@@ -12,8 +12,10 @@ const LazyLoading = () => {
         <button onClick={()=>setLoad('contact')}>Contact</button>
         <button onClick={()=>setLoad('login')}>Login</button>
 
+        <Suspense fallback={<h3>loading...</h3>}>
         {load=='contact' && <Contact/>}
         {load=='login' && <Login/>}
+        </Suspense>
     </div>
   )
 }
